a184a44ede77758173e970773b3084ecddb57db0
larryb02 <larrykbanks21@gmail.com>
2023-11-26 21:21:36 -0500

    TODO

--- utils.py
+++ utils.py
@@ -1,5 +1,14 @@
 import ffmpeg #not sure whats it called but you get what im doing here
 import time
 
+
+#TODO:
+#Resolve link function, i.e. take the query from discord "$play 'my favorite song'" get the link for that query
+#Stream function i.e. stream the source returned from ^
+#Pause...?
+#Queue Function (song queue)
+
+
+
 def getTimeMilis(): 
     return time.time() * 1000
