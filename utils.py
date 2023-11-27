3086fe4f61d40d8d1da3a990ad33139f58c367e5
larryb02 <77469185+larryb02@users.noreply.github.com>
2023-11-26 21:17:50 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 import ffmpeg #not sure whats it called but you get what im doing here
 
 
-def getTimeMilis(): #will possibly move this func somewhere else, doesnt make much sense here
+def getTimeMilis(): 
     return time.time() * 1000
