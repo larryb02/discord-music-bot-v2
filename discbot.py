2d709d6e725a8287e1ebf5b72d72718d2cf43d09
Larry Banks <larrykbanks21@gmail.com>
2024-05-08 21:50:32 -0400

    got streaming working, need to test a queue

--- discbot.py
+++ discbot.py
@@ -1,33 +1,29 @@
 import discord
 from discord.ext import commands
-import time 
+import time
 import os
 from dotenv import load_dotenv
 import logging
 import utils
-import os
 
 
-file = './secret.env' #THIS FILE DOESNT EXIST ANYMORE HAHAHAHAHA laptop broke >:(
+file = "./secret.env"
 load_dotenv(file)
 logging.basicConfig(level=logging.INFO)
+token = os.getenv("TOKEN")
+
+# TODO:
+# inactivity = leave channel
+# add other functionalities i.e. actually play song, pause, skip
 
 
-#TODO:
-#inactivity = leave channel
-#finish play command, probly need some helper functions (get link, stuff like that)
-#add other functionalities i.e. actually play song, pause, skip
-#also fix var naming convention/style need it to all be consistent
-
-
-token = os.getenv('TOKEN')
 class musicBot(commands.Bot):
+    Q = []
 
-    def __init__(self, *, command_prefix='$'):
+    def __init__(self, *, command_prefix="$"):
         intents = discord.Intents.default()
         intents.message_content = True
         super().__init__(command_prefix=command_prefix, intents=intents)
-        
 
 
 client = musicBot()
@@ -35,8 +31,31 @@ client = musicBot()
 
 @client.event
 async def on_ready():
-    print(f'{client.user} is online')
-'''
+    print(f"{client.user} is online")
+
+
+"""
+
+joinChannel:
+
+Function made so bot joins the channel that ctx.author is in
+
+"""
+
+
+async def joinChannel(ctx):
+    channel = ctx.author.voice.channel
+    print(f"{client.user} is joining {channel}!\n")
+    await (
+        channel.connect(
+            timeout=60.0, reconnect=False
+        )  # doesnt seem to handle inactivity, will do later
+    )
+    if ctx.voice_client is None:
+        ctx.voice_client.move_to(channel)
+
+
+"""
 play: plays music as queued by users
 
 implementation:
@@ -44,54 +63,38 @@ implementation:
 Need a queue to store songs
 Bot streams songs stored in queue
 if queue is empty bot can leave (may add a timer for this)
-'''
-@client.command(name='play')
-async def play(ctx, *, song):
-    songQ = [] #probly gonna make this a class attr or something, so i can use in other places
-    #bot joins user's channel
+
+TODO:
+
+sit down and think about how to handle this queue,
+
+"""
+
+
+@client.command(name="play")
+async def playSong(ctx, *, song):
+    # bot joins user's channel
     try:
-        channel = ctx.author.voice.channel
-        print(f"{client.user} is joining {channel}!\n")
-        await (channel.connect(timeout=60.0, reconnect=False) if ctx.voice_client is None else ctx.voice_client.move_to(channel))
+        await joinChannel(ctx)
+        # Do Q stuff
+        metaData = utils.resolveLink(song)
+        songQ = client.Q
+        songQ.append(metaData)  # for now add the entire dictionary
+        # play song, pop from q
+        cur = songQ.pop()
+        await ctx.send(f"Now Playing {cur['title']}!\n{cur['readableUrl']}")
+        src = discord.FFmpegOpusAudio(cur["streamableUrl"])
+        ctx.voice_client.play(src, after=None)
     except AttributeError:
-        await ctx.send(f"You must be in a voice channel to use this command!\ncurrent voice info: {ctx.author.voice}")
-        return
-    #add song to queue
-    songQ.append(song)
-    #streaming has its own caveats that i must handle
-    link = utils.resolveLink(songQ.pop())
-    ctx.send(f'Now Playing {link}')
-    os.sleep(3)
-    await ctx.voice_client.disconnect()
+        await ctx.send(f"You must be in a voice channel to use this command!")
+    # if list not empty
+    # playSong(ctx, song=songQ[-1])
 
 
+@client.command(name="leave")
+async def leaveChannel(ctx):
+    if ctx.voice_client is not None:
+        await ctx.voice_client.disconnect()
 
 
 client.run(token)
-
-
-
-
-     
-
-
-
-
-
-     
-     
-
-
-
-
-
-
-
-
-
-
-    
-
-   
-
-
--- utils.py
+++ utils.py
@@ -1,48 +1,51 @@
 import ffmpeg, time, yt_dlp
 
-#TODO:
+# TODO:
 # >:)
 
 
-def getTimeMilis(): #seems uneccessary now
+def getTimeMilis():  # seems uneccessary now
     return time.time() * 1000
 
-def stream(song):
-    infile = song #just pass the wp_url from resolveLink()
-    out, err = (ffmpeg
-    .input(infile)
-    .output('-', format='s16le', acodec='pcm_s16le', ac=1, ar='16k')
-    .overwrite_output()
-    .run(capture_stdout=True)
+
+def convertToStream(songLink):
+    out, err = (
+        ffmpeg.input(songLink)
+        .output("-", format="s16le", acodec="pcm_s16le", ac=1, ar="16k")
+        .overwrite_output()
+        .run(capture_stdout=True)
     )
     return out, err
 
-def resolveLink(query: str): 
+
+def resolveLink(query: str):
     ydl_opts = {
-        'format': 'bestaudio/best',
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'pcm_s16le',
-            'preferredquality': '192',
-        }],
-        'verbose': True
+        "format": "bestaudio/best",
+        "postprocessors": [
+            {
+                "key": "FFmpegExtractAudio",
+                "preferredcodec": "pcm_s16le",
+                "preferredquality": "192",
+            }
+        ],
+        "verbose": True,
+        "Quiet": True,
     }
-    #need to append ytsearch to strings
+    # need to append ytsearch to strings
     formattedQuery = f"ytsearch:{query}"
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         results = ydl.extract_info(formattedQuery, download=False)
-    print(f"Webpage URL: {results['entries'][0]['webpage_url']}\n")
-    #parts that i may want:
-    title = results['entries'][0]['title']
-    extractedUrl = results['entries'][0]['webpage_url']
-    returnedUrl = results['entries'][0]['url']
+    # parts that i may want:
+    title = results["entries"][0]["title"]
+    extractedUrl = results["entries"][0]["webpage_url"]
+    returnedUrl = results["entries"][0]["url"]
     info = {
-        'title': title,
-        'extractedUrl': extractedUrl,
-        'returnedUrl': returnedUrl
+        "title": title,
+        "readableUrl": extractedUrl,  # readable link
+        "streamableUrl": returnedUrl,  # streamable link
     }
     return info
-    
+
 
 if __name__ == "__main__":
     song = "BBL Drizzy"
